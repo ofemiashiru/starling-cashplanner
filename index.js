c17b74c72f378d125c59fad425290fb4a0ae2965
System Administrator <root@Oluwafemis-Computer.local>
2022-05-31 14:35:05 +0100

    4th commit

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ const app = express();
 //Set port to default port based on project server or 4000
 const port = process.env.PORT || 3000;
 
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
+
+
+passport.use(new OAuth2Strategy({
+    authorizationURL: process.env.OAUTH_API,
+    tokenURL: process.env.PERSONL_ACCESS_TOKEN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.OAUTH_REDIRECT_URI
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(err, profile);
+  }
+));
+
+passport.serializeUser((user, done)=>{
+    done(null, user);
+});
+
+passport.deserializeUser((user, done)=>{
+    done(null, user);
+});
 
 //process.env.NODE_ENV => production or undefined
 if(process.env.NODE_ENV === "production"){
@@ -19,9 +42,26 @@ if(process.env.NODE_ENV === "production"){
 
 app.route('/')
 .get((req, res)=>{
-    res.send('<h1>Hello</h1>')
+    res.send(`
+        <h1>Welcome to my App</h1> 
+        <a href="/auth">Authenticate</a>
+    `)
 })
 
+app.route('/auth')
+.get(passport.authenticate('oauth2', {scope: ['account']}))
+
+app.get('/auth/callback',
+  passport.authenticate('oauth2', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
+
+app.route('/dashboard')
+.get((req, res)=>{
+    res.send('Hello')
+})
 
 ///////////////////////////PORT LISTEN//////////////////////////////////////////
 app.listen(port, () => {
