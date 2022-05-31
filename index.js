1f8ed984dd34e75da7babf40de942082d6b12d83
System Administrator <root@Oluwafemis-Computer.local>
2022-05-31 17:08:35 +0100

    7th commit

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 //jshint esversion:6
 //Make sure you start this at the beginning of every project and use .gitignore too
-require("dotenv").config(); // This is to protect our codes, api keys etc.
+require('dotenv').config(); // This is to protect our codes, api keys etc.
 
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
+const session = require('express-session')
+const path = require('path');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
 //Make the app use express
 const app = express();
 
+app.use(session({secret:'cats'}));
+app.use(passport.initialize());
+app.use(passport.session())
 //Set port to default port based on project server or 4000
 const port = process.env.PORT || 3000;
 
-const passport = require('passport');
-const OAuth2Strategy = require('passport-oauth2');
+
+
+
+function isLoggedin(req, res, next){
+    req.user ?  next(): res.sendStatus(401)
+}
 
 
 passport.use(new OAuth2Strategy({
     authorizationURL: process.env.OAUTH_API,
-    tokenURL: process.env.PERSONL_ACCESS_TOKEN,
+    tokenURL: process.env.PRODUCTION_API,
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.OAUTH_REDIRECT_URI
   },
   function(accessToken, refreshToken, profile, cb) {
-    return cb(err, profile);
+    return cb(null, profile);
   }
 ));
 
@@ -49,7 +58,7 @@ app.route('/')
 })
 
 app.route('/auth')
-.get(passport.authenticate('oauth2', {scope: ['account']}))
+.get(passport.authenticate('oauth2', {scope:['account:read']}))
 
 
 app.route('/auth/callback')
@@ -61,11 +70,11 @@ app.route('/auth/callback')
 
 app.route('auth/failure')
 .get((req, res)=>{
-    res.send('<h1>Failed to Authenticate</h1>')
+    res.send('<h1>Something went wrong</h1>')
 })
 
 app.route('/dashboard')
-.get((req, res)=>{
+.get(isLoggedin,(req, res)=>{
     res.send('Hello')
 })
 
