bf1783165aef7f377c92a2171b43c2a26b9e7b25
System Administrator <root@Oluwafemis-Computer.local>
2022-06-01 02:17:50 +0100

    15th commit

--- index.js
+++ index.js
@@ -99,12 +99,11 @@ app.route('/auth/logout')
 
 app.route('/dashboard')
 .get(isLoggedin,(req, res) => {
-    console.log(req.user);
     res.send(
         `
         <h1>Hello</h1>
         <a href="/auth/logout">Log Out</a>
-        <p>${req.user}</p>
+        <p>${req.user.accountUid}</p>
         `
     );
 
