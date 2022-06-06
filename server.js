c8e60d5b2f83ff3a5fa78acf2470469c218c7e65
System Administrator <root@Oluwafemis-Computer.local>
2022-06-06 20:08:00 +0100

    getFeed

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.route('/dashboard')
             axios.get(`https://api-sandbox.starlingbank.com/api/v2/feed/account/${accountUid}/category/${categoryUid}?changesSince=${dateCreated}`, headers)
             .then((aResult)=>{
                 
-                console.log(aResult.data)
+                console.log(aResult)
 
                 res.send(
                     `
