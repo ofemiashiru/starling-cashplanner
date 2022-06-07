74dca803bc8de109a3e9be8153c2517c848d6da1
System Administrator <root@Oluwafemis-Computer.local>
2022-06-07 13:06:03 +0100

    grouppayments

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.route('/dashboard')
                     
                     return theFeed.reduce((acc, item)=> ({
                         ...acc,
-                        [item.spendingCategory] : acc[item.spendingCategory] += item.amount.minorUnits, 
+                        [item.spendingCategory] : acc[item.spendingCategory], 
                     }), {})
                 }
                 console.log(groupPayments(feed))
