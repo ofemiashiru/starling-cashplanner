82d86ae9edb90d2a04c907ba5db2fd44c5d86911
System Administrator <root@Oluwafemis-Computer.local>
2022-06-07 11:12:09 +0100

    getFeed

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.route('/dashboard')
                 const groupPayments = (theFeed) =>{
                     return theFeed.reduce((acc, item)=> ({
                         ...acc,
-                        [item.spendingCategory] : acc+=[item.amount.minorUnits], 
+                        [item.spendingCategory] : [acc.amount.minorUnits] += [item.amount.minorUnits], 
                     }), {})
                 }
                 console.log(groupPayments(feed))
