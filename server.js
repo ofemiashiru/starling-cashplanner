71850741a04bb5352a8d222d8bbac4fea2b0e0be
System Administrator <root@Oluwafemis-Computer.local>
2022-06-12 22:40:31 +0100

    grouppayments

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.route('/dashboard')
                 const totalOut =  groupedOutFeed.reduce(function (acc, obj) { return acc + obj.amount; }, 0);
 
                 const monthlySaving = totalIn - totalOut;
-                const savingInPercent = `${Math.round((monthlySaving/totalIn) * 100 + Number.EPSILON)}%`
+                const savingInPercent = `${Math.round((monthlySaving/totalIn + Number.EPSILON) * 100 )}%`
 
                 //Daily Calculations
                 const getDaysInMonth = (year, month) => {
