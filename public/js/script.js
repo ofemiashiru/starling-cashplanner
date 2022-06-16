f2554ab8f782cb4a0384d985f86a97dfe6052ebf
System Administrator <root@Oluwafemis-Computer.local>
2022-06-16 23:20:27 +0100

    FinalServer

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+
+function openIncome() {
+    document.getElementById("Income").style.height = "250px";
+}
+
+
+function openOutgoing() {
+    document.getElementById("Outgoing").style.height = "250px";
+}
+
+
+function openSaving() {
+    document.getElementById("Saving").style.height = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
+}
