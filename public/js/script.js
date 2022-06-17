0c4e7bc2ec1bd93382e6fc20c73bfb16c0340bf0
System Administrator <root@Oluwafemis-Computer.local>
2022-06-17 22:45:54 +0100

    FinalServer

--- public/js/script.js
+++ public/js/script.js
@@ -27,32 +27,3 @@ function closeSaving() {
     document.getElementById("Saving").style.height = "0";
 }
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-
-const config = {
-    type: 'line',   
-    data: data,
-    options: {}
-};
-
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
