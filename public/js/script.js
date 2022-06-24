9e2193be13bfb5c4a1cba11138259b91f6b63d4f
System Administrator <root@Oluwafemis-Computer.local>
2022-06-24 22:16:13 +0100

    FinalServer

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,16 @@
 
 function openIncome() {
-    document.getElementById("Income").style.height = "75%";
+    document.getElementById("Income").style.height = "80%";
 }
 
 
 function openOutgoing() {
-    document.getElementById("Outgoing").style.height = "75%";
+    document.getElementById("Outgoing").style.height = "80%";
 }
 
 
 function openSaving() {
-    document.getElementById("Saving").style.height = "75%";
+    document.getElementById("Saving").style.height = "80%";
 }
   
 
